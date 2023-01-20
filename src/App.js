37f3685471217f781756d7d21ef41e82fbbdc8dd
Thach Le <levu48@gmail.com>
2023-01-19 17:53:14 -0800

    new year notice

--- src/App.js
+++ src/App.js
@@ -59,7 +59,7 @@ const App = props => {
       <div style={styles.oneline}><a href="https://phobonsa.com">phobonsa.com</a>, (530) 965-5129, <a href='mailto:phobonsa@gmail.com'>phobonsa@gmail.com</a></div><br/>
       <div style={styles.oneline}>Reviews: <a href='https://goo.gl/maps/gkDqJ9L5cTNWCEcf9' target='_blank'>Google</a>, <a href='https://www.yelp.com/biz/pho-bonsa-chico?osq=Pho+Bonsa' target='_blank'>Yelp</a></div>
       <div style={styles.noticeHeader}><span>NOTICE:</span></div>
-      <div style={styles.notice}><span style={styles.tagLine}>We close on Sat Jan 21st for Lunar New Year</span><br/></div>
+      <div style={styles.notice}><span style={styles.tagLine}>We close on Fri-Sat Jan 20-21st for Lunar New Year</span><br/></div>
       </div>
 
       <Menu />
