51eaf82e7a8ef4d3560cf59eb93b192e0aaf1590
Thach Le <levu48@gmail.com>
2023-01-28 21:37:29 -0800

    local news

--- src/App.js
+++ src/App.js
@@ -57,7 +57,7 @@ const App = props => {
       <div style={styles.address}><span style={styles.tagLine}>"An Authentic Vietnamese Noodle Soup Restaurant"</span><br/>
       <div style={styles.oneline}><a href='https://goo.gl/maps/gkDqJ9L5cTNWCEcf9' target='_blank'>221 Normal Ave #B, Chico, CA 95928</a>, 11am - 8pm</div><br/>
       <div style={styles.oneline}><a href="https://phobonsa.com">phobonsa.com</a>, (530) 965-5129, <a href='mailto:phobonsa@gmail.com'>phobonsa@gmail.com</a></div><br/>
-      <div style={styles.oneline}>Reviews: <a href='https://goo.gl/maps/gkDqJ9L5cTNWCEcf9' target='_blank'>Google</a>, <a href='https://www.yelp.com/biz/pho-bonsa-chico?osq=Pho+Bonsa' target='_blank'>Yelp</a></div>
+      <div style={styles.oneline}>Reviews: <a href='https://goo.gl/maps/gkDqJ9L5cTNWCEcf9' target='_blank'>Google</a>, <a href='https://www.yelp.com/biz/pho-bonsa-chico?osq=Pho+Bonsa' target='_blank'>Yelp</a>, Local news: <a href='https://krcrtv.com/news/local/new-pho-restaurant-a-hit-for-chico-state-students' target='_blank'>ABC7 KRCR TV</a></div>
       {/* <div style={styles.noticeHeader}><span>NOTICE:</span></div>
       <div style={styles.notice}><span style={styles.tagLine}>We close on Fri-Sat Jan 20-21st for Lunar New Year</span><br/></div> */}
       </div>
