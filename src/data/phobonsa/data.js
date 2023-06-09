9be8a34c83415c8fddbf0eb66d312f9614f58049
Thach Le <levu48@gmail.com>
2023-06-09 07:43:41 -0700

    menu - 2023-06-09

--- src/data/phobonsa/data.js
+++ src/data/phobonsa/data.js
@@ -4,112 +4,191 @@ export const products = [
         name: 'Pho Dac Biet',
         description: 'special combination',
         prices: {
-            regular: 12.00,
-            small: 10.00
+            regular: 13.00,
+            small: 11.00
         }
     }, {
         id: '2',
         name: 'Pho Tai',
         description: 'steak',
         prices: {
-            regular: 12.00,
-            small: 10.00
+            regular: 13.00,
+            small: 11.00
         }
     }, {
         id: '3',
         name: 'Pho Bo-vien',
         description: 'meatball',
         prices: {
-            regular: 12.00,
-            small: 10.00
+            regular: 13.00,
+            small: 11.00
         }
     }, {
         id: '4',
         name: 'Pho Tai Nam',
         description: 'steak and flank',
         prices: {
-            regular: 12.00,
-            small: 10.00
+            regular: 13.00,
+            small: 11.00
         }
     }, {
         id: '5',
         name: 'Pho Tai Nam Gau',
         description: 'steak, flank and fatty flank',
         prices: {
-            regular: 12.00,
-            small: 10.00
+            regular: 13.00,
+            small: 11.00
         }
     }, {
         id: '6',
         name: 'Pho Tai Bo-vien',
         description: 'steak and meatball',
         prices: {
-            regular: 12.00,
-            small: 10.00
+            regular: 13.00,
+            small: 11.00
         }
     }, {
         id: '7',
         name: 'Pho Tai Chin',
         description: 'steak and brisket',
         prices: {
-            regular: 12.00,
-            small: 10.00
+            regular: 13.00,
+            small: 11.00
         }
     }, {
         id: '8',
         name: 'Pho Tai Gau',
         description: 'steak and fatty flank',
         prices: {
-            regular: 12.00,
-            small: 10.00
+            regular: 13.00,
+            small: 11.00
         }
     }, {
         id: '9',
         name: 'Pho Tai Gan',
         description: 'steak and tendon',
         prices: {
-            regular: 12.00,
-            small: 10.00
+            regular: 13.00,
+            small: 11.00
         }
     }, {
         id: '10',
         name: 'Pho Tai Chin Gan',
         description: 'steak, brisket and tendon',
         prices: {
-            regular: 12.00,
-            small: 10.00
+            regular: 13.00,
+            small: 11.00
         }
     }, {
         id: '11',
         name: 'Pho Ga',
         description: 'chicken',
         prices: {
-            regular: 12.00,
-            small: 10.00
+            regular: 13.00,
+            small: 11.00
         }
     }, {
         id: '12',
+        name: 'Pho Tom',
+        description: 'shrimp',
+        prices: {
+            regular: 13.00,
+            small: 11.00
+        }
+    }, {
+        id: '13',
+        name: 'Pho Chay',
+        description: 'vegan',
+        prices: {
+            regular: 13.00,
+            small: 11.00
+        }
+    }, {
+        id: '14',
+        name: 'Bao - Combo',
+        description: '',
+        prices: {
+            regular: 5.00,
+        }
+    }, {
+        id: '15',
+        name: 'Bao - BBQ Pork (2)',
+        description: '',
+        prices: {
+            regular: 5.00,
+        }
+    }, {
+        id: '16',
+        name: 'Bao - Chicken (2)',
+        description: '',
+        prices: {
+            regular: 5.00,
+        }
+    }, {
+        id: '17',
+        name: 'Bao - Sweet (2)',
+        description: '',
+        prices: {
+            regular: 5.00
+        }
+    }, {
+        id: '18',
         name: 'Spring Rolls (2)',
         description: '',
         prices: {
             regular: 5.00,
         }
     }, {
-        id: '13',
+        id: '19',
         name: 'Eggrolls (4)',
         description: '',
         prices: {
             regular: 6.00,
         }
     }, {
-        id: '14',
+        id: '20',
+        name: 'Dumplings (8)',
+        description: '',
+        prices: {
+            regular: 7.00,
+        }
+    }, {
+        id: '21',
+        name: 'Sugarcane Juice',
+        description: '',
+        prices: {
+            regular: 5.50,
+        }
+    }, {
+        id: '22',
+        name: 'Viet Iced Coffee',
+        description: '',
+        prices: {
+            regular: 4.50,
+        }
+    }, {
+        id: '23',
+        name: 'Thai Iced Tea',
+        description: '',
+        prices: {
+            regular: 4.50,
+        }
+    }, {
+        id: '24',
+        name: 'Hot or Iced Tea',
+        description: '',
+        prices: {
+            regular: 1.00,
+        }
+    }, {
+        id: '25',
         name: 'Water bottle',
         description: '',
         prices: {
             regular: 1.00,
         }
     }, {
-        id: '15',
+        id: '26',
         name: 'Soft drink',
         description: '',
         prices: {
